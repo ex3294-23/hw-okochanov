5ca15b718eed920d56c14e1f32cf4149d0015598
okochanov <ol.ko4v@yandex.ru>
2022-12-10 21:10:25 +0000

    HW2 review code

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,45 +1,160 @@
-package hw02unpackstring
+package unpkr
 
-import (
-	"errors"
-	"testing"
+import "testing"
 
-	"github.com/stretchr/testify/require"
-)
+func TestUnpack001(t *testing.T) {
+	var ps PackedString
+	var got, want string
 
-func TestUnpack(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected string
-	}{
-		{input: "a4bc2d5e", expected: "aaaabccddddde"},
-		{input: "abccd", expected: "abccd"},
-		{input: "", expected: ""},
-		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
-	}
+	ps = "a4bc2d5e"
+	want = "aaaabccddddde"
+	got = ps.Unpack()
 
-	for _, tc := range tests {
-		tc := tc
-		t.Run(tc.input, func(t *testing.T) {
-			result, err := Unpack(tc.input)
-			require.NoError(t, err)
-			require.Equal(t, tc.expected, result)
-		})
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
 	}
 }
 
-func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
-	for _, tc := range invalidStrings {
-		tc := tc
-		t.Run(tc, func(t *testing.T) {
-			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
-		})
+func TestUnpack002(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = "abcd"
+	want = "abcd"
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
 	}
 }
+
+func TestUnpack003(t *testing.T) {
+        var ps PackedString
+        var got, want string
+
+        ps = "3abc"
+        want = ""
+        got = ps.Unpack()
+
+        if got != want {
+                t.Errorf("ps.Unpack() == %q, want %q", got, want)
+        }
+}
+
+func TestUnpack004(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = "45"
+	want = ""
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
+	}
+}
+
+func TestUnpack005(t *testing.T) {
+        var ps PackedString
+        var got, want string
+
+        ps = "aaa10b"
+        want = ""
+        got = ps.Unpack()
+
+        if got != want {
+                t.Errorf("ps.Unpack() == %q, want %q", got, want)
+        }
+}
+
+func TestUnpack006(t *testing.T) {
+        var ps PackedString
+        var got, want string
+
+        ps = "aaa0b"
+        want = "aab"
+        got = ps.Unpack()
+
+        if got != want {
+                t.Errorf("ps.Unpack() == %q, want %q", got, want)
+        }
+}
+
+func TestUnpack007(t *testing.T) {
+        var ps PackedString
+        var got, want string
+
+        ps = " "
+        want = " "
+        got = ps.Unpack()
+
+        if got != want {
+                t.Errorf("ps.Unpack() == %q, want %q", got, want)
+        }
+}
+
+func TestUnpack008(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = `d\n5abc`
+	want = `d\n\n\n\n\nabc`
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
+	}
+}
+
+func TestUnpack009(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = `qwe\4\5`
+	want = `qwe45`
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
+	}
+}
+
+func TestUnpack010(t *testing.T) {
+        var ps PackedString
+        var got, want string
+
+        ps = `qwe\45`
+        want = `qwe44444`
+        got = ps.Unpack()
+
+        if got != want {
+                t.Errorf("ps.Unpack() == %q, want %q", got, want)
+        }
+}
+
+func TestUnpack011(t *testing.T) {
+        var ps PackedString
+        var got, want string
+
+        ps = `qwe\\5`
+        want = `qwe\\\\\`
+        got = ps.Unpack()
+
+        if got != want {
+                t.Errorf("ps.Unpack() == %q, want %q", got, want)
+        }
+}
+
+func TestUnpack012(t *testing.T) {
+        var ps PackedString
+        var got, want string
+
+        ps = `qw\ne`
+	want = " "
+        got = ps.Unpack()
+
+        if got != want {
+                t.Errorf("ps.Unpack() == %q, want %q", got, want)
+        }
+}
+
