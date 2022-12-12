6795b207c45842ece50b092c68bc2a10a57f8e72
okochanov <ol.ko4v@yandex.ru>
2022-12-12 21:23:05 +0000

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -5,51 +5,51 @@ import (
 	"strconv"
 	"strings"
 	"unicode"
-	"unicode/utf8"
 )
+
+const escapeSymbol string = "\\"
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(s string) (string, error) {
-	if len(s) == 0 {
-		return "", nil
-	}
+func Unpack(inputString string) (string, error) {
+	var resultBuilder strings.Builder
+	var targetToRepeat string
+	var nextSymbolEscaped bool
 
-	firstRune, _ := utf8.DecodeRuneInString(s)
-	if unicode.IsNumber(firstRune) {
+	for _, symbolRune := range inputString {
+		currentSymbol := string(symbolRune)
+		switch {
+		case nextSymbolEscaped:
+			if !(unicode.IsDigit(symbolRune) || currentSymbol == escapeSymbol) {
+				return "", ErrInvalidString
+			}
+			targetToRepeat = currentSymbol
+			nextSymbolEscaped = false
+
+		case currentSymbol == escapeSymbol:
+			resultBuilder.WriteString(targetToRepeat)
+			targetToRepeat = ""
+			nextSymbolEscaped = true
+
+		case unicode.IsDigit(symbolRune):
+			if targetToRepeat == "" {
+				return "", ErrInvalidString
+			}
+			repeatCount, err := strconv.Atoi(currentSymbol)
+			if err != nil {
+				return "", err
+			}
+			resultBuilder.WriteString(strings.Repeat(targetToRepeat, repeatCount))
+			targetToRepeat = ""
+
+		default:
+			resultBuilder.WriteString(targetToRepeat)
+			targetToRepeat = currentSymbol
+		}
+	}
+	if nextSymbolEscaped {
 		return "", ErrInvalidString
 	}
-	runeArr := []rune(s)
-	var result strings.Builder
-	var prev rune
-
-	for i, cur := range runeArr {
-		if i < 1 {
-			prev = cur
-			result.WriteRune(cur)
-			continue
-		}
-		if unicode.IsNumber(prev) && unicode.IsNumber(cur) {
-			return "", ErrInvalidString
-		}
-
-		if unicode.IsNumber(cur) {
-			if cur == 48 {
-				s := []rune(result.String())
-				s = s[:i-1]
-				result.Reset()
-				result.WriteString(string(s))
-				continue
-			}
-
-			cur, _ := strconv.ParseInt(string(cur), 10, 32)
-
-			result.WriteString(strings.Repeat(string(runeArr[i-1]), int(cur)-1))
-		} else {
-			result.WriteRune(cur)
-		}
-		prev = cur
-	}
-	return result.String(), nil
+	resultBuilder.WriteString(targetToRepeat)
+	return resultBuilder.String(), nil
 }
-
