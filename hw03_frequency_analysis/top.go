7371e39794be3ec1f7a446638d9dd7b33450bb6f
okochanov <ol.ko4v@yandex.ru>
2022-12-19 11:31:55 +0000

    HW2 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+)
+
+var r = regexp.MustCompile(`\s+`)
+
+func Top10(text string) []string {
+	if len(text) == 0 {
+		return nil
+	}
+
+	wordsMap := make(map[string]int)
+
+	words := r.Split(text, -1)
+	for _, word := range words {
+		wordsMap[word]++
+	}
+
+	result := make([]string, 0, len(wordsMap))
+	for name := range wordsMap {
+		result = append(result, name)
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		first := result[i]
+		second := result[j]
+
+		if wordsMap[first] == wordsMap[second] {
+			return first < second
+		}
+
+		return wordsMap[first] > wordsMap[second]
+	})
+
+	return result[:10]
 }
