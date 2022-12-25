fb060b45a50818676cfe09ea55cd3e9fb76fd95b
okochanov <ol.ko4v@yandex.ru>
2022-12-25 22:00:46 +0000

    HW3 is completed

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -1,9 +1,9 @@
 package hw03frequencyanalysis
 
 import (
-        "testing"
+	"testing"
 
-        "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 )
 
 // Change to true if needed.
@@ -44,39 +44,39 @@ var text = `Как видите, он  спускается  по  лестни�
                 В этот вечер...`
 
 func TestTop10(t *testing.T) {
-        t.Run("no words in empty string", func(t *testing.T) {
-                require.Len(t, Top10(""), 0)
-        })
+	t.Run("no words in empty string", func(t *testing.T) {
+		require.Len(t, Top10(""), 0)
+	})
 
-        t.Run("positive test", func(t *testing.T) {
-                if taskWithAsteriskIsCompleted {
-                        expected := []string{
-                                "а",         // 8
-                                "он",        // 8
-                                "и",         // 6
-                                "ты",        // 5
-                                "что",       // 5
-                                "в",         // 4
-                                "его",       // 4
-                                "если",      // 4
-                                "кристофер", // 4
-                                "не",        // 4
-                        }
-                        require.Equal(t, expected, Top10(text))
-                } else {
-                        expected := []string{
-                                "он",        // 8
-                                "а",         // 6
-                                "и",         // 6
-                                "ты",        // 5
-                                "что",       // 5
-                                "-",         // 4
-                                "Кристофер", // 4
-                                "если",      // 4
-                                "не",        // 4
-                                "то",        // 4
-                        }
-                        require.Equal(t, expected, Top10(text))
-                }
-        })
+	t.Run("positive test", func(t *testing.T) {
+		if taskWithAsteriskIsCompleted {
+			expected := []string{
+				"а",         // 8
+				"он",        // 8
+				"и",         // 6
+				"ты",        // 5
+				"что",       // 5
+				"в",         // 4
+				"его",       // 4
+				"если",      // 4
+				"кристофер", // 4
+				"не",        // 4
+			}
+			require.Equal(t, expected, Top10(text))
+		} else {
+			expected := []string{
+				"он",        // 8
+				"а",         // 6
+				"и",         // 6
+				"ты",        // 5
+				"что",       // 5
+				"-",         // 4
+				"Кристофер", // 4
+				"если",      // 4
+				"не",        // 4
+				"то",        // 4
+			}
+			require.Equal(t, expected, Top10(text))
+		}
+	})
 }
