22bc119944942c3326e57030866986a072430aca
okochanov <ol.ko4v@yandex.ru>
2022-12-25 18:30:20 +0000

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,39 +1,42 @@
 package hw03frequencyanalysis
 
 import (
-	"regexp"
-	"sort"
+        "fmt"
+        "regexp"
+        "sort"
 )
 
 var r = regexp.MustCompile(`\s+`)
 
 func Top10(text string) []string {
-	if len(text) == 0 {
-		return nil
-	}
+        if len(text) == 0 {
+                return nil
+        }
 
-	wordsMap := make(map[string]int)
+        wordsMap := make(map[string]int)
 
-	words := r.Split(text, -1)
-	for _, word := range words {
-		wordsMap[word]++
-	}
+        words := r.Split(text, -1)
+        for _, word := range words {
+                wordsMap[word]++
+        }
 
-	result := make([]string, 0, len(wordsMap))
-	for name := range wordsMap {
-		result = append(result, name)
-	}
+        result := make([]string, 0)
+        for name := range wordsMap {
+                result = append(result, name)
+        }
+        if len(wordsMap) <= len(result) {
+                sort.Slice(result, func(i, j int) bool {
+                        first := result[i]
+                        second := result[j]
 
-	sort.Slice(result, func(i, j int) bool {
-		first := result[i]
-		second := result[j]
+                        if wordsMap[first] == wordsMap[second] {
+                                return first < second
+                        }
 
-		if wordsMap[first] == wordsMap[second] {
-			return first < second
-		}
-
-		return wordsMap[first] > wordsMap[second]
-	})
-
-	return result[:10]
+                        return wordsMap[first] > wordsMap[second]
+                })
+        } else {
+                fmt.Print("Need more words")
+        }
+        return result[:10]
 }
