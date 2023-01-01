3e47d3c5eab8e8eaa319f20347912f9e12e58f6a
okochanov <ol.ko4v@yandex.ru>
2023-01-01 19:47:21 +0000

    fc

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -2,25 +2,97 @@ package hw04lrucache
 
 type List interface {
 	Len() int
-	Front() *ListItem
-	Back() *ListItem
-	PushFront(v interface{}) *ListItem
-	PushBack(v interface{}) *ListItem
-	Remove(i *ListItem)
-	MoveToFront(i *ListItem)
+	Front() *listItem
+	Back() *listItem
+	PushFront(v interface{}) *listItem
+	PushBack(v interface{}) *listItem
+	Remove(i *listItem)
+	MoveToFront(i *listItem)
 }
 
-type ListItem struct {
+type listItem struct {
 	Value interface{}
-	Next  *ListItem
-	Prev  *ListItem
+	Prev  *listItem
+	Next  *listItem
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	front  *listItem
+	back   *listItem
+	length int
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *listItem {
+	return l.front
+}
+
+func (l *list) Back() *listItem {
+	return l.back
+}
+
+func (l *list) PushBack(v interface{}) *listItem {
+	newItem := &listItem{
+		Value: v,
+		Prev:  nil,
+		Next:  l.back,
+	}
+
+	if l.back == nil {
+		l.front = newItem
+	} else {
+		l.back.Prev = newItem
+	}
+
+	l.back = newItem
+	l.length++
+	return newItem
+}
+
+func (l *list) PushFront(v interface{}) *listItem {
+	newItem := &listItem{
+		Value: v,
+		Prev:  l.front,
+		Next:  nil,
+	}
+
+	if l.front == nil {
+		l.back = newItem
+	} else {
+		l.front.Next = newItem
+	}
+	l.front = newItem
+	l.length++
+	return newItem
+}
+
+func (l *list) Remove(i *listItem) {
+	if i == nil {
+		return
+	}
+	l.length--
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.back = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.front = i.Prev
+	}
+}
+
+func (l *list) MoveToFront(i *listItem) {
+	l.Remove(i)
+	l.PushFront(i.Value)
 }
 
 func NewList() List {
-	return new(list)
+	return &list{}
 }
