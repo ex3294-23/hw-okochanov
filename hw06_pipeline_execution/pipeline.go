1581e122bc4bef4638a7903c32c1f32e5c924675
okochanov <ol.ko4v@yandex.ru>
2023-02-01 18:35:36 +0000

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,31 @@ type (
 
 type Stage func(in In) (out Out)
 
+func doneStage(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case prev, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- prev
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	pipe := in
+	for _, stage := range stages {
+		if stage != nil {
+			pipe = stage(doneStage(pipe, done))
+		}
+	}
+	return pipe
 }
