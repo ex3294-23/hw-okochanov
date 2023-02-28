09a6fe0ba2325244de4d9939400c2f9d70199dcb
okochanov <ol.ko4v@yandex.ru>
2023-02-28 10:21:12 +0000

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,17 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	err := Copy("testdata/input.txt", "out.txt", 10000, 100)
+	assert.Equal(t, err, ErrOffsetExceedsFileSize)
+
+	err = Copy("/home", "out.txt", 0, 0)
+	assert.Equal(t, err, ErrUnsupportedFile)
+
+	Copy("testdata/input.txt", "out.txt", 0, 10)
 }
